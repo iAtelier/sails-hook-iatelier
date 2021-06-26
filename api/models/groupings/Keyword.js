32d65374a6bd7d5c8207aced8dc7acf57939ff14
hossein <hossein@vitualizergalaxy.xyz>
2021-06-26 14:40:32 +0200

    fixing packages

--- api/models/groupings/Keyword.js
+++ api/models/groupings/Keyword.js
@@ -3,7 +3,7 @@ let Keyword = sails.hooks.borm.bookshelf.model('keyword', {
     tableName: 'keywords',
     hasTimestamps: true,
     keywordable() {
-        return this.morphTo('keywordable', ['keywordable_type', 'keywordable_id'], 'Post', 'Book');
+        return this.morphTo('keywordable', ['keywordable_type', 'keywordable_id'], 'Post', 'Book', 'Stuff');
     }
 });
 
